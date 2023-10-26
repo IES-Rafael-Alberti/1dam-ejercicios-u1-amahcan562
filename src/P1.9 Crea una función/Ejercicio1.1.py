3428ccc46244c0096be21b68f95d586baf5b2c67
amahcan562 <mcandres@iesrafaelalberti.es>
2023-10-26 16:46:52 +0200

    .

--- src/P1.9 Crea una función/Ejercicio1.1.py
+++
@@ -1,5 +0,0 @@
-def saludo(nombre):
-    return "Hola, " + nombre
-
-nombre = input("Introduce un nombre: ")
-print(saludo(nombre) + ".")
---
+++ src/P1.9 Crea una función/Ejercicio2.1_def.py
@@ -0,0 +1,8 @@
+nom = input("Escribe tu nombre: ")
+
+def nombre():
+    return "Hola, " + nom + "."
+
+print(nombre())
+
+
